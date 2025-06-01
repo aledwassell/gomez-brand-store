6eb4110bea92a0bc7e1b3fe42fc8d1e50f3f268d
aledwassell <aledwassell@googlemail.com>
2025-06-01 08:38:04 +0100

    added firebase functions and updaed the node version, this should work for deployment to firebase

--- app.config.ts
+++ app.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   ssr: true,
   server: {
-    preset: "node",
-    rollupConfig: {
-      external: ["__STATIC_CONTENT_MANIFEST"],
+    preset: "firebase",
+    firebase: {
+      gen: 2,
+      nodeVersion: "20",
     },
   },
 });
