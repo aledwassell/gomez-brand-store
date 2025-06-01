b43dfae9cef4c86dac06da720435960d9d9f544c
aledwassell <aledwassell@googlemail.com>
2025-06-01 08:01:34 +0100

    Update the firebase hosting pull req yml and the app config and the firebase json

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-    
+  ssr: true,
+  server: {
+    preset: "node",
+    rollupConfig: {
+      external: ["__STATIC_CONTENT_MANIFEST"],
+    },
+  },
 });
