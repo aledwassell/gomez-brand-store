1db1ab163b6a1fe7268ebf6eb17fa880379e74f5
aledwassell <aledwassell@googlemail.com>
2025-06-13 20:53:59 +0100

    Fix the home page product card layout

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,23 +3,24 @@ import { A } from "@solidjs/router";
 import { StoreProduct } from "~/models/printful/store.product.model";
 
 function ProductCard(props: StoreProduct) {
+  // Change the images up when they are hovered.
   const [isHovered, setIsHovered] = createSignal(false);
 
   return (
     <div
-      class="flex flex-col items-center"
+      class="flex flex-col lg:w-80"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <A href={`/${props.id}`} class="cursor-pointer">
+      <A href={`/${props.id}`} class="cursor-pointer w-full aspect-square">
         <img
           src={isHovered() ? props.thumbnail_url : props.thumbnail_url}
           alt={isHovered() ? "Hovered image" : "Default image"}
-          class="lg:w-80 aspect-square"
+          class="object-cover"
         />
       </A>
       <p class="my-4 w-full flex justify-between">
-        {props.name}
+        <span>{props.name}</span>
         <span class="text-gray-400">10.99</span>
       </p>
     </div>
