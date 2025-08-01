76d9db892b683a5b7eb4389b0f19eadb22f7b89e
aledwassell <aledwassell@googlemail.com>
2025-08-01 21:52:58 +0100

    Add Google ad sense script

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { Meta, MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { ErrorBoundary, onCleanup, onMount, Suspense } from "solid-js";
@@ -8,34 +8,16 @@ import ComingSoon from "./components/ComingSoon";
 import { appTitle } from "./constants/app-title";
 
 export default function App() {
-  let script: HTMLScriptElement;
   const showComingSoon = import.meta.env.MODE === "production";
-
-  onMount(() => {
-    const src =
-      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4783363180748954";
-    // Check if script already exists
-    const existingScript = document.querySelector(`script[src="${src}"]`);
-    if (existingScript) return;
-
-    script = document.createElement("script");
-    script.src = src;
-    script.async = true;
-    script.crossOrigin = 'anonymous'
-
-    document.head.appendChild(script);
-  });
-
-  onCleanup(() => {
-    if (script && script.parentNode) {
-      script.parentNode.removeChild(script);
-    }
-  });
 
   return (
     <Router
       root={(props) => (
         <MetaProvider>
+          <Meta
+            name="google-adsense-account"
+            content="ca-pub-4783363180748954"
+          />
           <Title>{appTitle}</Title>
           <div class="flex flex-col items-center bg-slate-800 min-h-screen text-white">
             {!showComingSoon && (
