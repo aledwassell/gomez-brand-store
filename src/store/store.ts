cebc1127e9bc91513696bb5e3bc833c6edceb618
aledwassell <aledwassell@googlemail.com>
2025-08-01 20:43:17 +0100

    Add checkout URl to the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,9 +3,11 @@ import { createStore } from "solid-js/store";
 const [store, setStore] = createStore<{
   shoppingCart: any[];
   isCartOpen: boolean;
+  checkoutUrl: string;
 }>({
   shoppingCart: [],
   isCartOpen: false,
+  checkoutUrl: "",
 });
 
 export { store, setStore };
