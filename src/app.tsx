629cd260a5ac2120781ff9897a11b70e1683f6ff
aledwassell <aledwassell@googlemail.com>
2025-08-01 21:47:40 +0100

    Add Google ad sense script

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,36 @@
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { ErrorBoundary, Suspense } from "solid-js";
+import { ErrorBoundary, onCleanup, onMount, Suspense } from "solid-js";
 import "./app.css";
 import QuickCart from "./components/QuickCart";
 import ComingSoon from "./components/ComingSoon";
 import { appTitle } from "./constants/app-title";
 
 export default function App() {
+  let script: HTMLScriptElement;
   const showComingSoon = import.meta.env.MODE === "production";
+
+  onMount(() => {
+    const src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4783363180748954";
+    // Check if script already exists
+    const existingScript = document.querySelector(`script[src="${src}"]`);
+    if (existingScript) return;
+
+    script = document.createElement("script");
+    script.src = src;
+    script.async = true;
+    script.crossOrigin = 'anonymous'
+
+    document.head.appendChild(script);
+  });
+
+  onCleanup(() => {
+    if (script && script.parentNode) {
+      script.parentNode.removeChild(script);
+    }
+  });
 
   return (
     <Router
