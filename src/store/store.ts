92005d4c370e473353514376f6ea86c195c1b031
aledwassell <aledwassell@googlemail.com>
2025-08-02 15:18:44 +0100

    Revert "Update the quick cart to handle products correctly and slightly more user friendily"
    This reverts commit b45aee65fb7b49a1c68dfccdf7a910f1b385beed.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,13 @@
 import { createStore } from "solid-js/store";
-import { BaseCartLine } from "~/models/Cart.model";
 
 const [store, setStore] = createStore<{
-    cart: BaseCartLine[];
-    isCartOpen: boolean;
-    checkoutUrl: string;
+  shoppingCart: any[];
+  isCartOpen: boolean;
+  checkoutUrl: string;
 }>({
-    cart: [],
-    isCartOpen: false,
-    checkoutUrl: "",
+  shoppingCart: [],
+  isCartOpen: false,
+  checkoutUrl: "",
 });
 
 export { store, setStore };
