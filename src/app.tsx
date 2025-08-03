e794dcf12fb65cfb5db0e3259cdb9c159fc10d54
aledwassell <aledwassell@googlemail.com>
2025-08-03 14:22:37 +0100

    Stop showing coming soon to test if the app works as it is on prod

--- src/app.tsx
+++ src/app.tsx
@@ -8,36 +8,34 @@ import ComingSoon from "./components/ComingSoon";
 import { appTitle } from "./constants/app-title";
 
 export default function App() {
-  const showComingSoon = import.meta.env.MODE === "production";
+    const showComingSoon = false;
+    //   const showComingSoon = import.meta.env.MODE === "production";
 
-  return (
-    <Router
-      root={(props) => (
-        <MetaProvider>
-          <Meta
-            name="google-adsense-account"
-            content="ca-pub-4783363180748954"
-          />
-          <Title>{appTitle}</Title>
-          <div class="flex flex-col items-center bg-slate-800 min-h-screen text-white">
-            {!showComingSoon && (
-              <header>
-                <ErrorBoundary fallback={(err) => err}>
-                  <QuickCart />
-                </ErrorBoundary>
-              </header>
+    return (
+        <Router
+            root={props => (
+                <MetaProvider>
+                    <Meta name="google-adsense-account" content="ca-pub-4783363180748954" />
+                    <Title>{appTitle}</Title>
+                    <div class="flex flex-col items-center bg-slate-800 min-h-screen text-white">
+                        {!showComingSoon && (
+                            <header>
+                                <ErrorBoundary fallback={err => err}>
+                                    <QuickCart />
+                                </ErrorBoundary>
+                            </header>
+                        )}
+                        <Suspense>
+                            <main class="flex-1 flex items-center justify-center">
+                                {showComingSoon ? <ComingSoon /> : props.children}
+                            </main>
+                        </Suspense>
+                        {!showComingSoon && <footer></footer>}
+                    </div>
+                </MetaProvider>
             )}
-            <Suspense>
-              <main class="flex-1 flex items-center justify-center">
-                {showComingSoon ? <ComingSoon /> : props.children}
-              </main>
-            </Suspense>
-            {!showComingSoon && <footer></footer>}
-          </div>
-        </MetaProvider>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+        >
+            <FileRoutes />
+        </Router>
+    );
 }
